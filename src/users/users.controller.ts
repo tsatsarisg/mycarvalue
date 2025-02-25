413b60e3404a2ac081f6e1607f74dd90fecd1712
George Tsatsaris <tsatsarisgiorgos@gmail.com>
2025-02-25 23:34:38 +0200

    [feat] auth service and user controller test

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,11 +15,11 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dtos';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
